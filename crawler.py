7b0ce5aa9d1222c6b310981a8600b1e0c9fcf13f
Yuriy Volodin <vjv@inbox.ru>
2020-11-14 17:35:18 +0300

    working increment

--- crawler.py
+++ crawler.py
@@ -6,9 +6,13 @@
 # Also we use next modules
 from bs4 import BeautifulSoup # Install it if you need: pip3 install beatifulsoup
 import csv
+from datetime import datetime, timedelta
+import locale
 import re
 import requests               # Install it if you need: pip3 install requests
 import time
+
+locale.setlocale(locale.LC_ALL, "")
 
 teacher = 'Володин+Юрий+Владимирович' # use + instead of space
 
@@ -25,21 +29,66 @@ html = get_html(rssu_url)
 soup = BeautifulSoup(html, 'lxml')
 
 trs = soup.find('div', class_="row collapse").find_all('tr')
-print(trs[1:4])
+
+begin_date = datetime.now()
+while begin_date.strftime("%A") != "понедельник":
+    begin_date -= timedelta(1)
+print('Begin of week: ', begin_date.strftime("%d/%m/%Y (%A)")) # begin of week
+end_date = begin_date + timedelta(14) # end of two week interval 
+
+dates2w = []
+for i in range(7,13):
+    dates2w.append((begin_date + timedelta(i)).strftime("%d.%m.%y"))
+print(dates2w)
+
+if "Нечетная неделя" == soup.find('div', class_="panel-green").find('p', class_="heading").text:
+    odd_week = True
+print('This week is odd: ', odd_week)    
+
+data = []
+for tr in trs[1:]:
+    cells = tr.find_all('td')
+    p = re.compile(r'\d{2}\.\d{2}\.\d{2}')
+    dates = p.findall(cells[3].text)
+    dates = list(set(dates2w).intersection(set(dates)))
+    if len(dates) > 0:
+        p = re.compile(r'[а-яА-ЯёЁ" "-]+')
+        discipline = p.findall(cells[3].text)[0].strip()
+        if discipline == 'Информатика и информационные технологии в профессиональной деятельности' \
+           or \
+           discipline == 'Информатика и основы информационно-коммуникационных технологий':
+            discipline_s = 'Инф. и ИТ'            
+        lesson_type = cells[4].text.strip()
+        if lesson_type.lower() == 'лабораторная работа':
+            lesson_type = 'лаба'
+        if lesson_type.lower() == 'практическая работа':
+            lesson_type = 'практика'
+        location = cells[5].text.strip()
+        group = cells[6].text.strip()
+        p = re.compile(r'\d+')
+        hmhm = p.findall(cells[1].text.strip())
+        lesson_time = ['', '']
+        try:
+            for i in range(2):
+                lesson_time[i] += hmhm[2*i]+':'+hmhm[1 + 2*i]
+        except:
+            print('Something went wrong!!! Bad time format: [ {} ]'.format(cells[1].text))
+            print('See timetable and manually correct time for:', cells[0].text, cells[2].text, group)
+            lesson_time = ['8:15', '22:05']
+            
+        for date in dates:
+            data.append([date, lesson_time[0], date, lesson_time[1],\
+                         location, group + ' ' + discipline, \
+                         discipline_s + ' ' + lesson_type])
+
 today = time.strftime('%d.%m')
-f = open('calendar_'+today+'.csv', 'w')
+f = open('calendar_'+today+'.csv', 'w', newline='', encoding='utf8')
 with f:
     writer = csv.writer(f)
     writer.writerow(['Start Date', 'Start Time', 'End Date', 'End Time', 'Location', 'Description','Subject'])
+    writer.writerows(data)
 
-data = []
-for tr in trs[1:4]:
-    cells = tr.find_all('td')
-    data.append(cells)
-    print(cells[1].get_text())
 
-p = re.compile(r'\d+')
-print(p.findall('12 drummers drumming, 11 pipers piping, 10 lords a-leaping'))
 
     
 
