64f39002ccf4008967312857b8aa4b5aa7fa88fe
Yuriy Volodin <vjv@inbox.ru>
2020-12-02 20:36:53 +0300

    edit to follow pep8

--- crawler.py
+++ crawler.py
@@ -5,13 +5,14 @@
 # ==================== Version 1.31 ===========================================
 # We use lxml parser, install it if you need: pip3 install lxml
 # Also we use next modules
-from bs4 import BeautifulSoup # Install it if you need: pip3 install beatifulsoup
+from bs4 import BeautifulSoup  # Install it if you need: pip3 install bs4
 import csv                    # Install it if you need: pip3 install csv
 from datetime import datetime, timedelta
 import locale
 import re
 import requests               # Install it if you need: pip3 install requests
-import sys, time
+import sys
+import time
 
 locale.setlocale(locale.LC_ALL, "")
 
@@ -37,17 +38,19 @@ except(IOError, OSError) as e:
     print()
     sys.exit('Error when reading settings.txt !!! Check also file encoding.')
     
+
 def get_html(url):
-    r = requests.get(url) # Response
+    r = requests.get(url)  # Response
     if r.status_code != 200:
         print("Can't get page. Check connection to rgsu.net and try to restart this script.")
-    return r.text # Return html page code
+    return r.text  # Return html page code
+
 
 rssu_url = 'https://rgsu.net/for-students/timetable/timetable.html?template=&action=index&admin_mode=&nc_ctpl=935&Teacher='
 rssu_url += teacher 
 html = get_html(rssu_url)
 
-soup = BeautifulSoup(html, 'lxml')
+soup = BeautifulSoup(html, 'html.parser')
 
 trs = soup.find('div', class_="row collapse").find_all('tr')
 
@@ -55,9 +58,9 @@ trs = soup.find('div', class_="row collapse").find_all('tr')
 date_now = datetime.now()
 while date_now.strftime("%A") != "понедельник":
     date_now -= timedelta(1)
-print('Begin of current week: ', date_now.strftime("%d/%m/%Y (%A)")) # begin of week
+print('Begin of current week: ', date_now.strftime("%d/%m/%Y (%A)"))  # begin of week
 # Sunday belongs to next week on this site
-#oddness = 
+# oddness =
 if "Нечетная неделя" == soup.find('div', class_="panel-green").find('p', class_="heading").text:
     odd_week = True
 else:
@@ -68,11 +71,9 @@ print('This week is odd: ', odd_week)
 # -----------------------------------------------
 
 date_sett = []
-while(begin_date <= end_date):
+while begin_date <= end_date:
     date_sett.append(begin_date.strftime("%d.%m.%y"))
     begin_date += timedelta(1)
-#print('Days in selected interval:')
-#print(date_sett)
 
 data = []
 for tr in trs[1:]:
@@ -81,7 +82,7 @@ for tr in trs[1:]:
     dates = p.findall(cells[3].text)
     dates = list(set(date_sett).intersection(set(dates)))
     if len(dates) > 0:
-        p = re.compile(r'[а-яА-ЯёЁ" "-]+')
+        p = re.compile(r'[а-яА-ЯёЁ -]+')
         discipline = p.findall(cells[3].text)[0].strip()
         if len(discipline) > 16:
             discipline_s = ''.join([(w if len(w) == 1 else w[0].upper()) for w in discipline.split(' ')])
@@ -108,14 +109,13 @@ for tr in trs[1:]:
             lesson_time = ['8:00', '22:00']
             
         for date in dates:
-            data.append([date, lesson_time[0], lesson_time[1],\
-                         location, group, discipline, lesson_type, \
-                         discipline_s + ' ' + lesson_type_s])
+            data.append([date, lesson_time[0], lesson_time[1], location, group,
+                         discipline, lesson_type, discipline_s + ' ' + lesson_type_s])
     
 datalines = []
 for i in range(len(data)):
     j = i + 1
-    while(j < len(data)):
+    while j < len(data):
         if data[i][0] == data[j][0] and data[i][1] == data[j][1] and data[i][7] == data[j][7]:
             if data[i][3] != data[j][3]:
                 data[j][3] += ' / ' + data[i][3]
@@ -123,7 +123,7 @@ for i in range(len(data)):
             break
         j += 1
     else:
-        datalines.append([data[i][0], data[i][1], data[i][0], data[i][2], data[i][3], \
+        datalines.append([data[i][0], data[i][1], data[i][0], data[i][2], data[i][3],
                           data[i][4] + ': ' + data[i][5] + ', ' + data[i][6], data[i][7]])
 
 print('Load in selected period equals {0} hours. It is {1:.1f} hours per week in average.'.format(2 * len(datalines), 7 * 2 * len(datalines) / max(1, len(date_sett))))
@@ -132,8 +132,7 @@ f_name = 'calendar_' + time.strftime('%d.%m') + '.csv'
 f = open(f_name, 'w', newline='', encoding='utf8')
 with f:
     writer = csv.writer(f)
-    writer.writerow(['Start Date', 'Start Time', 'End Date', 'End Time', 'Location', 'Description','Subject'])
+    writer.writerow(['Start Date', 'Start Time', 'End Date', 'End Time', 'Location', 'Description', 'Subject'])
     writer.writerows(datalines)
-print('================================================================================')
-print('OK! Timetible was done - see file [' + f_name + '] in this directory.\
-\nImport it to your Google Calendar.')
+print('=' * 80)
+print('OK! Timetable was done - see file [' + f_name + '] in this directory.\nImport it to your Google Calendar.')
